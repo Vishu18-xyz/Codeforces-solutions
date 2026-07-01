023c7901a19751a3f29dcc60d23746c5935d943a
Vishu18-xyz <pqndey9179@gmail.com>
2026-07-01 10:58:41 +0530

    Add Jellyfish and Undertale [A] from Codeforces

---
+++ 1875/A - Jellyfish and Undertale/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        long long a, b;
+        int n;
+        cin >> a >> b >> n;
+ 
+        long long ans = b;
+ 
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+ 
+            if (x < a - 1)
+                ans += x;
+            else
+                ans += (a - 1);
+        }
+ 
+        cout << ans << endl;
+    }
+ 
+    return 0;
+}
