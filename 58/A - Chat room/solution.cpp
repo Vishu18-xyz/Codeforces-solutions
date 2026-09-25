84d863f0732a4377dbcba6bf49dab6613985dbc4
Vishesh Pandey <pqndey9179@gmail.com>
2026-09-25 07:00:34 +0530

    Add Chat room [A] from Codeforces

---
+++ 58/A - Chat room/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+ 
+    string target ="hello";
+    int j=0;
+ 
+    for(int i = 0; i<s.length(); i++) {
+        if(s[i] == target[j]) {
+            j++;
+ 
+            if(j == 5) {
+                cout<<"YES";
+                return 0;
+            }
+        }
+    }
+ 
+    cout<<"NO";
+ 
+    return 0;
+}
